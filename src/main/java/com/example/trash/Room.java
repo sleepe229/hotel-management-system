3b58216d197db8d3ec0e2b206ba71c384172b0e3
Stephen <tpyngg@gmail.com>
2023-12-01 02:06:35 +0300

    added funcionality for order rooms for clients, started dev funcs for hotel porter

--- src/main/java/com/example/trash/Room.java
+++ src/main/java/com/example/trash/Room.java
@@ -5,12 +5,26 @@ public class Room {
     String type;
     String status;
     int number;
+    String clientlogin;
+    String fullname;
+    String phonenumber;
+    String email;
 
     public Room(int id, String type, String status, int number) {
         this.id = id;
         this.type = type;
         this.status = status;
         this.number = number;
+    }
+
+    public Room(int id, int number, String fullname, String clientlogin, String phonenumber, String email, String status) {
+        this.id = id;
+        this.status = status;
+        this.number = number;
+        this.fullname = fullname;
+        this.clientlogin = clientlogin;
+        this.phonenumber = phonenumber;
+        this.email = email;
     }
 
     public int getId() {
@@ -35,6 +49,38 @@ public class Room {
 
     public void setStatus(String status) {
         this.status = status;
+    }
+
+    public String getClientlogin() {
+        return clientlogin;
+    }
+
+    public void setClientlogin(String clientlogin) {
+        this.clientlogin = clientlogin;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public int getNumber() {
